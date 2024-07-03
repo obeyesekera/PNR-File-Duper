51e0ccc44d33061b71c7fd1a94a1c2bdfcc349aa
Nuwan Obeyesekera <nuwan.obeyesekera@scicom.com.my>
2024-07-04 01:37:16 +0530

    Add project files.

---
+++ PNR-File-Duper/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace PNR_File_Gen
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtFirstFlight = new System.Windows.Forms.TextBox();
+            this.txtFlightCount = new System.Windows.Forms.TextBox();
+            this.lblSourceFile = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpArrivalTime = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFlightPrefix = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDelayTime = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpDepartureTime = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(112, 34);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtFirstFlight
+            // 
+            this.txtFirstFlight.Location = new System.Drawing.Point(252, 41);
+            this.txtFirstFlight.Name = "txtFirstFlight";
+            this.txtFirstFlight.Size = new System.Drawing.Size(150, 31);
+            this.txtFirstFlight.TabIndex = 1;
+            this.txtFirstFlight.TextChanged += new System.EventHandler(this.txtFirstFlight_TextChanged);
+            // 
+            // txtFlightCount
+            // 
+            this.txtFlightCount.Location = new System.Drawing.Point(162, 15);
+            this.txtFlightCount.Name = "txtFlightCount";
+            this.txtFlightCount.Size = new System.Drawing.Size(150, 31);
+            this.txtFlightCount.TabIndex = 2;
+            this.txtFlightCount.TextChanged += new System.EventHandler(this.txtFlightCount_TextChanged);
+            // 
+            // lblSourceFile
+            // 
+            this.lblSourceFile.AutoSize = true;
+            this.lblSourceFile.Location = new System.Drawing.Point(145, 17);
+            this.lblSourceFile.Name = "lblSourceFile";
+            this.lblSourceFile.Size = new System.Drawing.Size(111, 25);
+            this.lblSourceFile.TabIndex = 3;
+            this.lblSourceFile.Text = "lblSourceFile";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.dtpDepartureTime);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dtpArrivalTime);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtFlightPrefix);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtFirstFlight);
+            this.panel1.Location = new System.Drawing.Point(13, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(478, 162);
+            this.panel1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Departure Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Flight Number";
+            // 
+            // dtpArrivalTime
+            // 
+            this.dtpArrivalTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpArrivalTime.Location = new System.Drawing.Point(161, 78);
+            this.dtpArrivalTime.Name = "dtpArrivalTime";
+            this.dtpArrivalTime.Size = new System.Drawing.Size(150, 31);
+            this.dtpArrivalTime.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Arrival Time";
+            // 
+            // txtFlightPrefix
+            // 
+            this.txtFlightPrefix.Location = new System.Drawing.Point(161, 41);
+            this.txtFlightPrefix.Name = "txtFlightPrefix";
+            this.txtFlightPrefix.Size = new System.Drawing.Size(85, 31);
+            this.txtFlightPrefix.TabIndex = 5;
+            this.txtFlightPrefix.TextChanged += new System.EventHandler(this.txtFlightPrefix_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Flight";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Flight Count";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(379, 349);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(112, 34);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.txtDelayTime);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.txtFlightCount);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(13, 239);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(478, 95);
+            this.panel2.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(253, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 25);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "(minutes)";
+            // 
+            // txtDelayTime
+            // 
+            this.txtDelayTime.Location = new System.Drawing.Point(162, 52);
+            this.txtDelayTime.Name = "txtDelayTime";
+            this.txtDelayTime.Size = new System.Drawing.Size(85, 31);
+            this.txtDelayTime.TabIndex = 5;
+            this.txtDelayTime.TextChanged += new System.EventHandler(this.txtDelayTime_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Delay";
+            // 
+            // dtpDepartureTime
+            // 
+            this.dtpDepartureTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpDepartureTime.Location = new System.Drawing.Point(161, 114);
+            this.dtpDepartureTime.Name = "dtpDepartureTime";
+            this.dtpDepartureTime.Size = new System.Drawing.Size(150, 31);
+            this.dtpDepartureTime.TabIndex = 10;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 395);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblSourceFile);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "PNR File Duper";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtFirstFlight;
+        private System.Windows.Forms.TextBox txtFlightCount;
+        private System.Windows.Forms.Label lblSourceFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtFlightPrefix;
+        private System.Windows.Forms.DateTimePicker dtpArrivalTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDelayTime;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtpDepartureTime;
+    }
+}
+
---
+++ PNR-File-Duper/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PNR_File_Gen
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private string xDocFile = "";
+        private string pnrPath = "";
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Title = "Select Source PNR File";
+            ofd.DefaultExt = "xml";
+            ofd.Filter = "PNR files (*.xml)|*.xml|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lblSourceFile.Text = ofd.SafeFileName;
+                xDocFile = ofd.FileName;
+                pnrPath = Path.GetDirectoryName(xDocFile);
+
+                btnLoad.Enabled = false;
+                fieldEnabler(true);
+            }
+        }
+
+        private void fieldEnabler(bool status)
+        {
+            txtFirstFlight.Enabled = status;
+            txtFlightCount.Enabled = status;
+            txtFlightPrefix.Enabled = status;
+            dtpArrivalTime.Enabled = status;
+            dtpDepartureTime.Enabled = status;
+            txtDelayTime.Enabled = status;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            btnLoad.Enabled = false;
+            btnGenerate.Enabled = false;
+            fieldEnabler(false);
+                        
+            int startFlifht = int.Parse(txtFirstFlight.Text);
+            int flightCount = int.Parse(txtFlightCount.Text);
+            int endFlight = startFlifht + flightCount;
+            int delayTime = int.Parse(txtDelayTime.Text);
+
+            DateTime newArrivalTime = dtpArrivalTime.Value;
+            DateTime newDepartureTime = dtpDepartureTime.Value;
+
+            for (int i = startFlifht; i < endFlight; i++)
+            {
+                if (i > startFlifht)
+                {
+                    newArrivalTime = newArrivalTime.AddMinutes(delayTime);
+                    newDepartureTime = newDepartureTime.AddMinutes(delayTime);
+                }
+
+                string newFlight = txtFlightPrefix.Text + i;
+                string newArrivalTimeString = newArrivalTime.ToString("yyyy-MM-dd")+ "T" +newArrivalTime.ToString("HH:mm:ss");
+                string newDepartureTimeString = newDepartureTime.ToString("yyyy-MM-dd") + "T" + newDepartureTime.ToString("HH:mm:ss");
+
+                generateNewXML(newFlight, newArrivalTimeString, newDepartureTimeString);
+            }
+
+            MessageBox.Show("Completed");
+            Application.Exit();
+
+        }
+
+        private void generateNewXML(string nFlight, string nArrival, string nDeparture)
+        {
+            try
+            {
+                string fileLoc = pnrPath;
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xDocFile);
+
+                changeXMLVal(doc, "FlightNumber", nFlight);
+                changeXMLVal(doc, "ArrivalDateTime", nArrival);
+                changeXMLVal(doc, "DepartureDateTime", nDeparture);
+
+                doc.Save(pnrPath + "\\" + nFlight + ".xml");
+                doc = null;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                /*
+                 * Possible Exceptions:
+                 *  System.ArgumentException
+                 *  System.ArgumentNullException
+                 *  System.InvalidOperationException
+                 *  System.IO.DirectoryNotFoundException
+                 *  System.IO.FileNotFoundException
+                 *  System.IO.IOException
+                 *  System.IO.PathTooLongException
+                 *  System.NotSupportedException
+                 *  System.Security.SecurityException
+                 *  System.UnauthorizedAccessException
+                 *  System.UriFormatException
+                 *  System.Xml.XmlException
+                 *  System.Xml.XPath.XPathException
+                */
+            }
+        }
+
+        private XmlDocument changeXMLVal(XmlDocument doc, string element, string value)
+        {
+            XmlNode node = doc.SelectSingleNode("/root/FlightDetails/" + element);
+            if (node != null)
+            {
+                node.InnerText = value;
+            }
+            else
+            {
+                XmlNode root = doc.DocumentElement;
+                XmlElement elem;
+                elem = doc.CreateElement(element);
+                elem.InnerText = value;
+                root.AppendChild(elem);
+            }
+
+            return doc;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblSourceFile.Text = "Source file not selected";
+            btnGenerate.Enabled = false;
+            fieldEnabler(false);
+        }
+
+        private void txtFlightPrefix_TextChanged(object sender, EventArgs e)
+        {
+            validateGenerateBtn();
+        }
+
+        private void txtFirstFlight_TextChanged(object sender, EventArgs e)
+        {
+            validateGenerateBtn();
+        }
+
+        private void txtFlightCount_TextChanged(object sender, EventArgs e)
+        {
+            validateGenerateBtn();
+        }
+
+        private void validateGenerateBtn()
+        {
+            if ((txtFlightPrefix.TextLength > 0)&&
+                (txtFirstFlight.TextLength > 0)&&
+                (txtFlightCount.TextLength > 0)&&
+                (txtDelayTime.TextLength > 0))
+            {
+                btnGenerate.Enabled = true;
+            }
+            else
+            {
+                btnGenerate.Enabled = false;
+            }
+        }
+
+        private void txtGroundTime_TextChanged(object sender, EventArgs e)
+        {
+            validateGenerateBtn();
+        }
+
+        private void txtDelayTime_TextChanged(object sender, EventArgs e)
+        {
+            validateGenerateBtn();
+        }
+    }
+}
